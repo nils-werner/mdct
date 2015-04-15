f1a88eaddcca0d218087f4e8533e7c164c094e6f
Nils Werner <nils.werner@audiolabs-erlangen.de>
2015-04-15 12:33:10 +0200

    More docs

--- docs/index.rst
+++ docs/index.rst
@@ -8,6 +8,15 @@ Contents:
 
    mdct
 
+Wikipedia says about the MDCT:
+
+The modified discrete cosine transform (MDCT) is a lapped transform based on the type-IV discrete cosine transform (DCT-IV), with the additional property of being lapped: it is designed to be performed on consecutive blocks of a larger dataset, where subsequent blocks are overlapped so that the last half of one block coincides with the first half of the next block.
+
+This overlapping, in addition to the energy-compaction qualities of the DCT, makes the MDCT especially attractive for signal compression applications, since it helps to avoid artifacts stemming from the block boundaries.
+
+As a result of these advantages, the MDCT is employed in most modern lossy audio formats, including MP3, AC-3, Vorbis, Windows Media Audio, ATRAC, Cook, and AAC.
+
+The implementation used here is based on the FFT wrapper published in [Borsi].
 
 Indices and tables
 ==================
@@ -15,3 +24,7 @@ Indices and tables
 * :ref:`genindex`
 * :ref:`modindex`
 * :ref:`search`
+
+
+.. [Borsi] Marina Bosi, Richard E. Goldberg and Leonardo Chiariglione,
+   "Introduction to Digital Audio Coding and Standards", Kluwer Academic Publishers, 01 December, 2002.
--- mdct/__init__.py
+++ mdct/__init__.py
@@ -5,6 +5,19 @@ import stft
 
 
 def mdct(x):
+    """ Calculate MDCT of input signal
+
+    Parameters
+    ----------
+    x : array_like
+        The input signal
+
+    Returns
+    -------
+    out : array_like
+        The output signal
+
+    """
     N = len(x)
     n0 = (N / 2 + 1) / 2
 
@@ -20,6 +33,19 @@ def mdct(x):
 
 
 def imdct(X):
+    """ Calculate inverse MDCT of input signal
+
+    Parameters
+    ----------
+    X : array_like
+        The input signal
+
+    Returns
+    -------
+    out : array_like
+        The output signal
+
+    """
     N = 2 * len(X)
     n0 = (N / 2 + 1) / 2
 
@@ -39,17 +65,73 @@ def imdct(X):
     )
 
 
-def spectrogram(x):
+def spectrogram(
+    x,
+    framelength=1024,
+    window=None
+):
+    """ Calculate windowed MDCT of input signal
+
+    Parameters
+    ----------
+    x : array_like
+        The input signal
+    framelength : int
+        Framesize for :code:`stft.spectrogram`. Defaults to 1024.
+    window : array_like
+        Window for :code:`stft.ispectrogram`.
+        Defaults to :code:`scipy.signal.cosine`.
+
+    Returns
+    -------
+    out : array_like
+        The output signal
+
+    See Also
+    --------
+    mdct : MDCT
+
+    """
     return stft.spectrogram(
         x,
         halved=False,
+        framelength=framelength,
+        window=window,
         transform=mdct
     )
 
 
-def ispectrogram(X):
+def ispectrogram(
+    X,
+    framelength=1024,
+    window=None
+):
+    """ Calculate lapped inverse MDCT of input signal
+
+    Parameters
+    ----------
+    x : array_like
+        The input signal
+    framelength : int
+        Framesize for :code:`stft.ispectrogram`. Defaults to 1024.
+    window : array_like
+        Window for :code:`stft.ispectrogram`.
+        Defaults to :code:`scipy.signal.cosine`.
+
+    Returns
+    -------
+    out : array_like
+        The output signal
+
+    See Also
+    --------
+    imdct : inverse MDCT
+
+    """
     return stft.ispectrogram(
         X,
         halved=False,
+        framelength=framelength,
+        window=window,
         transform=imdct
     )
