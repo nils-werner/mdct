d000a8e8f7cb7ec302f250221c3f438dfd04f24e
Nils Werner <nils.werner@audiolabs-erlangen.de>
2015-04-16 09:34:47 +0200

    Cleaned up setup.py comments

--- setup.py
+++ setup.py
@@ -4,34 +4,21 @@ import setuptools
 
 if __name__ == "__main__":
     setuptools.setup(
-        # Name of the project
         name='mdct',
-
-        # Version
         version='0.2',
 
-        # Description
         description='A fast MDCT implementation using SciPy and FFTs',
-
-        # Your contact information
         author='Nils Werner',
         author_email='nils.werner@gmail.com',
 
-        # License
         license='MIT',
-
-        # Packages in this project
-        # find_packages() finds all these automatically for you
         packages=setuptools.find_packages(),
 
-        # Dependencies, this installs the entire Python scientific
-        # computations stack
         install_requires=[
             'numpy>=1.6',
             'scipy>=0.13.0',
             'stft',
         ],
-
         extras_require={
             'tests': [
                 'pytest',
@@ -60,6 +47,7 @@ if __name__ == "__main__":
             'Topic :: Multimedia :: Sound/Audio :: Analysis',
             'Topic :: Multimedia :: Sound/Audio :: Sound Synthesis'
         ],
+
         zip_safe=True,
         include_package_data=True,
     )
